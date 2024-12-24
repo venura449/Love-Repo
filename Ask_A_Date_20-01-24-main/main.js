5ef8fbd8269e57491e5703ccb270be0a9bba80d4
J.A Venura Jayaasingha <137485589+venura449@users.noreply.github.com>
2024-12-24 23:20:10 +0530

    Update main.js

--- Ask_A_Date_20-01-24-main/main.js
+++ Ask_A_Date_20-01-24-main/main.js
@@ -4,6 +4,7 @@ const imageTwo = document.querySelector(".image-2");
 const btnYes = document.querySelector(".btn-yes");
 const btnNo = document.querySelector(".btn-no");
 const text = document.querySelector(".text-2");
+const text1 = document.querySelector(".text-3");
 
 function getRandomNumber(min, max) {
   // Calculate the random number between min and max (inclusive)
@@ -39,6 +40,7 @@ btnYes.addEventListener("click", (e) => {
   imageOne.classList.add("hide");
   imageTwo.classList.remove("hide");
   text.classList.remove("hide");
+  text1.classList.remove("hide");
   setTimeout(()=>{
     window.location.href = "mailto:Vidaththeekshana@gmail.com?subject=%F0%9F%92%8C%20You%E2%80%99ve%20Made%20My%20Day!%20%F0%9F%92%95&body=Hi%20%2C%0A%0AWow%2C%20I%20can%E2%80%99t%20stop%20smiling%20right%20now!%20%F0%9F%98%84%20Hearing%20you%20say%20yes%20just%20made%20my%20day%2C%20my%20week%2C%20my%20everything!%20%F0%9F%8C%9F%0A%0AI%E2%80%99m%20super%20excited%20for%20us%20to%20spend%20time%20together%2C%20and%20I%20can%E2%80%99t%20wait%20to%20make%20it%20special.%20%F0%9F%8E%89%E2%9C%A8%20Let%E2%80%99s%20plan%20something%20amazing%E2%80%94you%20deserve%20it!%20%F0%9F%A5%B0%0A%0ALet%20me%20know%20what%20works%20for%20you%2C%20and%20I%E2%80%99ll%20make%20sure%20it%E2%80%99s%20perfect.%20%F0%9F%97%93%EF%B8%8F%F0%9F%92%AB%0A%0AThanks%20for%20making%20me%20the%20happiest%20person%20today!%20%F0%9F%A5%B3%F0%9F%92%96%0A%0AWarmly%2C%0AMal%20Kumari%20%F0%9F%98%8A%F0%9F%92%8C";
   },10000)
